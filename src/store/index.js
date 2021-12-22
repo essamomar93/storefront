f7902a6a9001c3403f2e194b2733f40cfd21a7c2
essam omar <essam.93@hotmail.com>
2021-12-22 19:21:28 +0200

    lab38

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
-import { combineReducers, createStore } from "redux"
+import { combineReducers, createStore,applyMiddleware  } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
 import productsReducer from "./product"
 import cartReducer from "./cart"
+import thunk from '../middileware/thunk'
+import productReducerAPI from './productAPI'
 
 let reducers = combineReducers({
     store: productsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    products:productReducerAPI
 });
 
 const store = () => {
-    return createStore(reducers, composeWithDevTools());
+    return createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 }
 
 export default store();
